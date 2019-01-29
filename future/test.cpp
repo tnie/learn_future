145a2faa7bb763c8190c10d00864c2ceaef1e350
nielyd <nielong2014@gmail.com>
2019-01-29 14:14:03 +0800

    README.md

--- future/test.cpp
+++ future/test.cpp
@@ -7,9 +7,24 @@ using namespace std;
 int main()
 {
     auto t1 = std::chrono::steady_clock::now();
-    auto task = std::async( launch::deferred,   []() {
-        std::this_thread::sleep_for(1s);
+    auto task = std::async( /*launch::deferred,*/   []() {
+        std::this_thread::sleep_for(3s);
     });
+    // 启用 deferred 执行策略，会导致死循环
+    std::future_status status;
+    do {
+        status = task.wait_for(std::chrono::seconds(1));
+        if (status == std::future_status::deferred) {
+            std::cout << "deferred\n";
+        }
+        else if (status == std::future_status::timeout) {
+            std::cout << "timeout\n";
+        }
+        else if (status == std::future_status::ready) {
+            std::cout << "ready!\n";
+        }
+    } while (status != std::future_status::ready);
+    //
     std::this_thread::sleep_for(2s);
     task.get();
     auto t2 = std::chrono::steady_clock::now();
