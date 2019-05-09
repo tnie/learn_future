b39c5c942b169a15dd9b867290dcd2fbeeb07cc4
nielyd <nielong2014@gmail.com>
2019-05-09 12:38:01 +0800

    理解程序输出为什么和直觉不一致

--- std_future/test.cpp
+++ std_future/test.cpp
@@ -1,6 +1,7 @@
 #include <future>
 #include <chrono>
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 using namespace std;
 
@@ -13,10 +14,18 @@ int fun_async()
         this_thread::sleep_for(2s);
         return 2;
     }));
-    cout << __NOW_IN_MILLISECONDS_ << "\n"  // TODO why print 09:00:02 then 09:00:00
-        << f1.get() << "\n"
-        << __NOW_IN_MILLISECONDS_
-        << endl;
+    {
+        auto t1 = __NOW_IN_MILLISECONDS_;
+        auto n = f1.get();
+        auto t2 = __NOW_IN_MILLISECONDS_;
+        spdlog::info("{}\n {}\n {} ", t1, n, t2);
+    }
+    //https://blog.csdn.net/me4weizhen/article/details/52541325 函数参数执行顺序 左 <- 右
+    //spdlog::info("{}\n {}\n {} ", __NOW_IN_MILLISECONDS_, f1.get(), __NOW_IN_MILLISECONDS_);
+    //cout << __NOW_IN_MILLISECONDS_ << "\n"  // TODO why print 09:00:02 then 09:00:00
+    //    << f1.get() << "\n"
+    //    << __NOW_IN_MILLISECONDS_
+    //    << endl;
     return 0;
 }
 
@@ -43,8 +52,24 @@ int fun_task()
     return 0;
 }
 
+#include <algorithm>
 int main()
 {
+    future<void> f3;
+    for (size_t i = 0; i < 5; i++)
+    {
+        f3 = std::async(launch::async, [i]() {
+            this_thread::sleep_for(5s);
+            spdlog::info("niel: {}", i);
+        });
+        // 重点理解输出，同时连续输出两次：
+        //函数调用后，移动赋值操作延迟到 f3 之前的任务完成后才发生
+    }
+    auto f2 = std::async(launch::async, []() {
+        this_thread::sleep_for(3s);
+        spdlog::info("liyw");
+    });
+    return fun_async();
     promise<int> p1;    //std::promise 只应当使用一次。
     auto f1 = p1.get_future();
     std::thread([&p1] {
